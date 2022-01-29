ccdec3668a63405df3de44913f27e021f93bc302
TheNova22 <jayantsogikar@gmail.com>
2022-01-29 13:01:48 +0530

    File seperation

--- college_helper/lib/home/screens/homePage.dart
+++ college_helper/lib/home/screens/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:college_helper/home/utils/color_gen.dart';
+import 'package:college_helper/home/widgets/collegeCard.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -120,90 +121,6 @@ class _MyHomePageState extends State<MyHomePage> {
             color: Color(0xff505050),
           ),
         ), // This trailing comma makes auto-formatting nicer for build methods.
-      ),
-    );
-  }
-}
-
-class CollegeCard extends StatelessWidget {
-  const CollegeCard({
-    Key? key,
-    required this.width,
-    required this.title,
-    required this.address,
-    required this.imgUrl,
-  }) : super(key: key);
-
-  final double width;
-  final String title;
-  final String address;
-  final String imgUrl;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 10),
-      width: width - 20,
-      height: 150,
-      decoration: BoxDecoration(
-          color: colorGetter(title),
-          borderRadius: const BorderRadius.all(Radius.circular(20))),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0),
-            child: Image.network(
-              imgUrl,
-              width: width / 2 - 20,
-              height: 150,
-              fit: BoxFit.fitHeight,
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                padding: const EdgeInsets.only(right: 10.0),
-                width: width / 2 - 40,
-                child: AutoSizeText(
-                  title,
-                  textAlign: TextAlign.end,
-                  minFontSize: 12,
-                  maxFontSize: 33,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: GoogleFonts.raleway(
-                      textStyle: const TextStyle(
-                          fontSize: 33, color: Color(0xff565656))),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                color: Colors.black54,
-                alignment: Alignment.centerRight,
-                height: 2,
-                width: width / 4,
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                padding: const EdgeInsets.only(right: 10.0),
-                width: width / 2 - 40,
-                child: AutoSizeText(
-                  address,
-                  textAlign: TextAlign.end,
-                  minFontSize: 11,
-                  maxFontSize: 16,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: GoogleFonts.raleway(
-                      textStyle: const TextStyle(
-                          fontSize: 33, color: Color(0xff565656))),
-                ),
-              ),
-            ],
-          )
-        ],
       ),
     );
   }
