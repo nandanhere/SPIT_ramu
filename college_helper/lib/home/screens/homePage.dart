61eb38dbd62b3f1fcb2d6491f7b90069f889d9ea
TheNova22 <jayantsogikar@gmail.com>
2022-01-29 12:58:35 +0530

    Added college cards

--- college_helper/lib/home/screens/homePage.dart
+++ college_helper/lib/home/screens/homePage.dart
@@ -59,6 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               children: <Widget>[
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
                       'Colleges Nearby',
@@ -68,51 +69,42 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 15,
                 ),
-                Container(
-                  width: width - 20,
-                  height: 150,
-                  decoration: BoxDecoration(
-                      color: colorGetter("RIT"),
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(20))),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 20.0),
-                        child: Image.network(
-                          'https://www.iesonline.co.in/colleges-image/ramaiah-institute-of-technology.jpg',
-                          width: width / 2 - 20,
-                          height: 150,
-                          fit: BoxFit.fitHeight,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10.0),
-                        child: Column(
-                          children: [
-                            Container(
-                              width: width / 2 - 40,
-                              child: AutoSizeText(
-                                "Ramaiah Institute Of Technology",
-                                textAlign: TextAlign.end,
-                                minFontSize: 15,
-                                maxFontSize: 33,
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                style: GoogleFonts.raleway(
-                                    textStyle: const TextStyle(
-                                        fontSize: 33,
-                                        color: Color(0xff565656))),
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
+                CollegeCard(
+                  width: width,
+                  title: "Ramaiah Institute Of Technology",
+                  address: "MSR Nagar, Bangalore",
+                  imgUrl:
+                      'https://www.iesonline.co.in/colleges-image/ramaiah-institute-of-technology.jpg',
+                ),
+                CollegeCard(
+                  width: width,
+                  title: "RV College Of Engineering",
+                  address: "RR Nagar, Bangalore",
+                  imgUrl:
+                      'https://www.iesonline.co.in/colleges-image/ramaiah-institute-of-technology.jpg',
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: ElevatedButton(
+                    child: Text(
+                      "Find More",
+                      style: GoogleFonts.raleway(
+                          textStyle: const TextStyle(
+                              fontSize: 16, color: Color(0xff565656))),
+                    ),
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blue[100]!),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side:
+                                        BorderSide(color: Colors.red[200]!)))),
                   ),
                 )
               ],
@@ -120,10 +112,98 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
         floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.deepOrange[100],
           onPressed: () {},
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
+          tooltip: 'Chat With Us',
+          child: const Icon(
+            Icons.message_outlined,
+            color: Color(0xff505050),
+          ),
         ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class CollegeCard extends StatelessWidget {
+  const CollegeCard({
+    Key? key,
+    required this.width,
+    required this.title,
+    required this.address,
+    required this.imgUrl,
+  }) : super(key: key);
+
+  final double width;
+  final String title;
+  final String address;
+  final String imgUrl;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      width: width - 20,
+      height: 150,
+      decoration: BoxDecoration(
+          color: colorGetter(title),
+          borderRadius: const BorderRadius.all(Radius.circular(20))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Image.network(
+              imgUrl,
+              width: width / 2 - 20,
+              height: 150,
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                padding: const EdgeInsets.only(right: 10.0),
+                width: width / 2 - 40,
+                child: AutoSizeText(
+                  title,
+                  textAlign: TextAlign.end,
+                  minFontSize: 12,
+                  maxFontSize: 33,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: GoogleFonts.raleway(
+                      textStyle: const TextStyle(
+                          fontSize: 33, color: Color(0xff565656))),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                color: Colors.black54,
+                alignment: Alignment.centerRight,
+                height: 2,
+                width: width / 4,
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                padding: const EdgeInsets.only(right: 10.0),
+                width: width / 2 - 40,
+                child: AutoSizeText(
+                  address,
+                  textAlign: TextAlign.end,
+                  minFontSize: 11,
+                  maxFontSize: 16,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: GoogleFonts.raleway(
+                      textStyle: const TextStyle(
+                          fontSize: 33, color: Color(0xff565656))),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
--- college_helper/lib/home/utils/color_gen.dart
+++ college_helper/lib/home/utils/color_gen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 //https://api.flutter.dev/flutter/material/Colors/accents-constant.html
-const cols = Colors.accents;
 
 // Gets the color from a string
 Color colorGetter(String title) {
+  List cols = Colors.accents;
+  // cols.remove(Colors.deepPurpleAccent);
   int total = 0;
   for (int i = 0; i < title.length; i++) {
     if (title[i] != ' ') {
@@ -15,5 +16,5 @@ Color colorGetter(String title) {
   // .shade100 to make it lighter
   HSLColor val = HSLColor.fromColor(cols[total % cols.length].shade100);
   //made a slight variation - suraj, feel free to tweak
-  return val.withLightness(0.91).withSaturation(0.8).toColor();
+  return val.withLightness(0.93).withSaturation(0.8).toColor();
 }
